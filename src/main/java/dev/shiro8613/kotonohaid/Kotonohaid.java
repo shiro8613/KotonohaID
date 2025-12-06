c24762672d9a5816e4843892f7a36975b597b586
toms0910 <psp1070@gmail.com>
2025-12-07 03:56:11 +0900

    init and complete

---
+++ src/main/java/dev/shiro8613/kotonohaid/Kotonohaid.java
@@ -0,0 +1,10 @@
+package dev.shiro8613.kotonohaid;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Kotonohaid implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/dev/shiro8613/kotonohaid/client/KotonohaidClient.java
@@ -0,0 +1,12 @@
+package dev.shiro8613.kotonohaid.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.command.argument.ItemStackArgumentType;
+
+public class KotonohaidClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
